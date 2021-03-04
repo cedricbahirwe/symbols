7ac43c49d771ff4decf7fb14310cfc2d508455c1
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-03-04 22:03:25 +0200

    Add Search/Filter

--- MySymbols/MySymbols/RestrictedSymbolsView.swift
+++ MySymbols/MySymbols/RestrictedSymbolsView.swift
@@ -15,24 +15,46 @@ fileprivate struct RestrictedSymbol:Decodable, Hashable {
 
 struct RestrictedSymbolsView: View {
     private var restrictedSymbols: [RestrictedSymbol] = Bundle.main.decode([RestrictedSymbol].self, from: "restrictedSymbols.json").filter({ !$0.symbolName.hasSuffix(".rtl")})
+    
+    @State private var searchedSymbol = ""
+    @State private var isSearching: Bool = false
+    
     var body: some View {
         NavigationView {
-            ScrollView {
-                ForEach(restrictedSymbols, id: \.self) { restrictedSymbol in
-                    VStack {
-                        Image(systemName: restrictedSymbol.symbolName)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                        Text(restrictedSymbol.feature)
-                            .minimumScaleFactor(0.65)
+            VStack {
+                ScrollView {
+                    Spacer()
+                        .frame(height: 45)
+                    ForEach(
+                        isSearching ?
+                            restrictedSymbols.filter({$0.symbolName.lowercased().contains(searchedSymbol.lowercased())})
+                            : restrictedSymbols
+                        , id: \.self) { restrictedSymbol in
+                        VStack {
+                            Image(systemName: restrictedSymbol.symbolName)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .zIndex(-1)
+                            Text(restrictedSymbol.feature)
+                                .minimumScaleFactor(0.65)
+                        }
+                        .font(.system(size: 30, weight: .bold, design: .rounded))
                     }
-                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .zIndex(5)
                 }
+                .padding(.horizontal, 10)
+                .navigationBarTitle("Restricted Symbols")
             }
-            .padding(.horizontal, 10)
-            .navigationBarTitle("Restricted Symbols")
+            .frame(maxWidth: .infinity)
+            .overlay(SearchingView($searchedSymbol) , alignment: .top)
+            .onChange(of: searchedSymbol, perform: checkSearch)
         }
-        
+    }
+    
+    func checkSearch(_ value: String) {
+        withAnimation {
+            isSearching = !value.isEmpty
+        }
     }
 }
 
--- MySymbols/MySymbols/SymbolsView.swift
+++ MySymbols/MySymbols/SymbolsView.swift
@@ -15,26 +15,43 @@ struct SymbolsView: View {
             GridItem(.flexible()),
             GridItem(.flexible())
         ]
+    @State private var searchedSymbol = ""
+    @State private var isSearching: Bool = false
+    
     var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(symbols, id:\.self) { symbol in
-                        VStack {
-                            Image(systemName: symbol)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                            Text(symbol)
-                                .lineLimit(2)
-                                .minimumScaleFactor(0.5)
+            VStack {
+                ScrollView {
+                    LazyVGrid(columns: columns) {
+                        ForEach(
+                            isSearching ?
+                                symbols.filter({$0.lowercased().contains(searchedSymbol.lowercased())})
+                                : symbols
+                            , id:\.self) { symbol in
+                            VStack {
+                                Image(systemName: symbol)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                Text(symbol)
+                                    .lineLimit(2)
+                                    .minimumScaleFactor(0.5)
+                            }
+                            .font(.system(size: 30, weight: .bold, design: .rounded))
                         }
-                        .font(.system(size: 30, weight: .bold, design: .rounded))
                     }
+                    .padding(.horizontal, 10)
+                    
                 }
-                .padding(.horizontal, 10)
-                
+                .navigationBarTitle("Symbols")
             }
-            .navigationBarTitle("Symbols")
+            .frame(maxWidth: .infinity)
+            .overlay(SearchingView($searchedSymbol) , alignment: .top)
+            .onChange(of: searchedSymbol, perform: checkSearch)
+        }
+    }
+    func checkSearch(_ value: String) {
+        withAnimation {
+            isSearching = !value.isEmpty
         }
     }
 }
@@ -42,5 +59,49 @@ struct SymbolsView: View {
 struct SymbolsView_Previews: PreviewProvider {
     static var previews: some View {
         SymbolsView()
+    }
+}
+
+
+struct SearchingView: View {
+    @Binding var searchedSymbol: String
+    
+    init(_ term: Binding<String>) {
+        _searchedSymbol = term
+    }
+    var body: some View {
+        HStack(spacing: 5) {
+            HStack {
+                TextField("Type somethin'", text: $searchedSymbol)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(false)
+                    .padding(.leading, 8)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(Color(.label))
+                        .frame(width: 40, height: 40)
+                })
+            }
+            .frame(height: 40)
+            .border(Color(.label))
+            .font(.system(size: 20, weight: .semibold, design: .rounded))
+            if !searchedSymbol.isEmpty {
+                Button(action: {
+                    withAnimation {
+                        searchedSymbol = ""
+                    }
+                }, label: {
+                    Text("Cancel")
+                        .foregroundColor(Color(.darkGray))
+                })
+            }
+        }
+        .padding(.horizontal)
+        .padding(.bottom, 3)
+        .background(Color(.systemBackground).opacity(0.9))
+        
     }
 }
